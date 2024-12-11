bd7ff413724e89e7e2b839b04b68d51710475f82
OPM007756 <yuki.shishido@optim.co.jp>
2024-12-11 22:37:19 +0900

    CRUDのCの部分の実装

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
+type Todo struct {
+	ID   	  int    `json:"id"`
+	Body      string `json:"body"`
+	Completed bool   `json:"completed"`
+}
+
 func main() {
-	r := gin.Default()
+	fmt.Println("Hello World")
+	app := fiber.New()
+	
+	todos := []Todo{}
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Welcome to the Go Backend!",
-		})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
 	})
+	
+	// Todoを作成するためのエンドポイント
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		// Bodyが空の時にJSONでエラーを出力する
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
 
-	r.Run(":3000")
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Satatus(201).JSON(todo)
+	})	
+
 }
